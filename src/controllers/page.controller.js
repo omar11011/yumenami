8f687172f64a2dfadda6afaaad6c08960a91f74f
omar11011 <atarama11011@gmail.com>
2024-10-12 13:57:59 -0500

    Añadiendo página de resultados

--- src/controllers/page.controller.js
+++ src/controllers/page.controller.js
@@ -22,7 +22,6 @@ const FormPage = (req, res) => {
 
 const DataPage = async (req, res) => {
     const datos = await Survey.find({}).select('-createdAt -updatedAt -__v').lean()
-    console.log(datos.length)
     try {
         createFolder()
         convertToJson(datos)
@@ -42,8 +41,51 @@ const DataPage = async (req, res) => {
     })
 }
 
+const ResultPage = async (req, res) => {
+    let results = JSON.parse(JSON.stringify(questions))
+    let schools = []
+    let datos = await Survey.find({}).select('-createdAt -updatedAt -__v').lean()
+    
+    results.forEach(e => {
+        e.questions = e.questions.map(q => {
+            return {
+                question: q,
+            }
+        })
+    })
+
+    datos.forEach((e, idx) => {
+        for (let i = 1; i <= 30; i++) {
+            let section = parseInt(i / 5)
+            if (i % 5 === 0) section -= 1
+            let question = i - 1 - section * 5
+            if (!results[section].questions[question].percentage) results[section].questions[question].percentage = 0
+            if (e[`P${i}`] === true) results[section].questions[question].percentage += 1
+            if (idx === datos.length - 1) {
+                results[section].questions[question].percentage = (results[section].questions[question].percentage * 100 / datos.length).toFixed(2)
+            }
+        }
+        let school = schools.findIndex(f => f.name === e.school)
+        if (school < 0) schools.push({
+            name: e.school,
+            type: e.typeOfSchool,
+            province: e.province,
+            count: 1,
+        })
+        else schools[school].count += 1
+    })
+
+    console.log(schools)
+    return res.render('results', {
+        navbar: 'results',
+        results,
+        schools,
+    })
+}
+
 module.exports = {
     HomePage,
     FormPage,
     DataPage,
+    ResultPage,
 }
--- src/routes/page.routes.js
+++ src/routes/page.routes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express')
-const { HomePage, FormPage, DataPage } = require('../controllers/page.controller')
+const { HomePage, FormPage, DataPage, ResultPage } = require('../controllers/page.controller')
 
 const router = Router()
 
 router.get('/', HomePage)
 router.get('/form', FormPage)
 router.get('/data', DataPage)
+router.get('/results', ResultPage)
 
 module.exports = router
